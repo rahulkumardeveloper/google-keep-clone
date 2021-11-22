30576e9b08e9022977b895dd865429c16b0ce98c
Rahul Kumar Gautam <gautamrahul995@gmail.com>
2021-11-22 14:05:04 +0530

    first commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="header">
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/R_logo.svg/2560px-R_logo.svg.png"
      alt="_logo.png" width="70px" height="40px"/>
-     <h1>Google Keep Clone</h1>
+     <h1>Google Keep App</h1>
 
     </div>
     </>
